b49128a2a0943863caa6e33e05131e4bcf5eb45f
dale-conklin <conklin.dale@gmail.com>
2020-10-21 13:19:00 +0000

    Configure kafka container and configure git to ignore virtual environment

--- docker-files/startup.sh
+++ docker-files/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/kafka-2.6.0-src/bin/zookeeper-server-start.sh config/zookeeper.properties &
-/opt/kafka-2.6.0-src/bin/kafka-server-start.sh config/server.properties &
+/opt/kafka-2.6.0-src/bin/zookeeper-server-start.sh /opt/kafka-2.6.0-src/config/zookeeper.properties &
+/opt/kafka-2.6.0-src/bin/kafka-server-start.sh /opt/kafka-2.6.0-src/config/server.properties
