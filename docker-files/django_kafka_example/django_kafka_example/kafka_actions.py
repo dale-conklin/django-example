27eeb0b38c1ee891981df41494afb9edbd8dbb50
dale-conklin <conklin.dale@gmail.com>
2020-10-21 20:28:13 +0000

    create and list topics in kafka

--- docker-files/django_kafka_example/django_kafka_example/kafka_actions.py
+++ docker-files/django_kafka_example/django_kafka_example/kafka_actions.py
@@ -2,6 +2,7 @@ import kafka
 from kafka.admin import KafkaAdminClient, NewTopic
 
 KAFKA_SERVER = 'kafka-svr:9092'
+CLIENT_ID='dale-id'
 
 class k_consumer(kafka.KafkaConsumer):
     def __init__(self):
@@ -10,13 +11,13 @@ class k_consumer(kafka.KafkaConsumer):
         super().__init__()
 
 def get_kafka_topics():
-    consumer = kafka.KafkaConsumer(bootstrap_servers=[KAFKA_SERVER])
+    consumer = kafka.KafkaConsumer(client_id=CLIENT_ID, bootstrap_servers=[KAFKA_SERVER])
 
     return consumer.topics()
 
 def create_kafka_topic(new_topic: str):
     if len(new_topic) > 0:
-        admin_client = KafkaAdminClient(bootstrap_servers=[KAFKA_SERVER])
+        admin_client = KafkaAdminClient(client_id=CLIENT_ID, bootstrap_servers=[KAFKA_SERVER])
         topic_list = []
         topic_list.append(NewTopic(name=f"{new_topic}", num_partitions=1, replication_factor=1))
         admin_client.create_topics(new_topics=topic_list, validate_only=False)
--- docker-files/startup.sh
+++ docker-files/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 /opt/kafka-2.6.0-src/bin/zookeeper-server-start.sh /opt/kafka-2.6.0-src/config/zookeeper.properties &
+sleep 10
 /opt/kafka-2.6.0-src/bin/kafka-server-start.sh /opt/kafka-2.6.0-src/config/server.properties
